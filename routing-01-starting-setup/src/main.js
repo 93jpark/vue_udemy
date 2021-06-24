68c410b2ae2816d934629233195c6adaff78f68d
joon <joon@joons-MacBook-Pro.local>
2021-06-24 15:45:57 +0900

    router setting

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -1,7 +1,22 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {path: '/teams',component: TeamsList},// our-domain.com/teams => TeamsList
+        {path: '/users',component: UsersList},// our-domain.com/teams => TeamsList
+        {path: '/teams/:teamId', component: TeamMembers},
+    ]
+});
 
 const app = createApp(App)
+
+app.use(router); // use() method for using third-party package
 
 app.mount('#app');
