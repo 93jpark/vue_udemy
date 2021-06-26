657be32a09407d59e68db5b4e5f8e96cb586a6af
joon <joon@joons-MacBook-Pro.local>
2021-06-26 23:07:31 +0900

    save

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -5,13 +5,21 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        {path: '/teams',component: TeamsList},// our-domain.com/teams => TeamsList
-        {path: '/users',component: UsersList},// our-domain.com/teams => TeamsList
-        {path: '/teams/:teamId', component: TeamMembers},
+        // { path: '/', component: TeamsList }
+        { path: '/', redirect: '/teams' },
+        // { path: '/teams',component: TeamsList, alias: '/' }
+        { name:'teams', path: '/teams',component: TeamsList, children: [
+                { name:'team-members', path: ':teamId', component: TeamMembers, props: true } // /teams/t1
+            ] 
+        },
+        { path: '/users',component: UsersList },// our-domain.com/teams => TeamsList
+        // { path: '/teams/:teamId', component: TeamMembers, props: true }, // :teamId is route parameter
+        { path: '/:notFound(.*)', component: NotFound },
     ]
 });
 
