83b1614ed4b84eb869950fb669d436b0a08c70a7
Joonwoo Park <joon@Joonwoos-MacBook-Pro.local>
2021-07-10 18:17:57 +0900

    working on vuex

---
+++ vuex-01-starting-setup/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <base-container title="Vuex">
+    <the-counter></the-counter>
+    <change-counter></change-counter>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
+import ChangeCounter from './components/ChangeCounter.vue';
+
+export default {
+  components: {
+    BaseContainer,
+    TheCounter,
+    ChangeCounter
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ vuex-01-starting-setup/src/components/ChangeCounter.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <button @click="addOne">Add 1</button>
+        <button @click="addNum">Add +</button>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        addOne() {
+            this.$store.commit('increment');
+        },
+        addNum() {
+            this.$store.commit('increase', {
+                    type: number,
+                    value: 10
+                }  
+            );
+        }
+        
+    }
+}
+</script>
