9318d8225916a117df3975b5088de56001280d45
Joonwoo Park <joon@Joonwoos-MacBook-Pro.local>
2021-07-27 23:36:24 +0900

    fill up components with dummy

--- vue-main-project/src/pages/coaches/CoachesList.vue
+++ vue-main-project/src/pages/coaches/CoachesList.vue
@@ -1,10 +1,16 @@
 <template>
+  <div>
     <section>
         FILTER
     </section>
     <section>
-        LIST OF COACHES
+      <div class="controls">
+        <button>Refresh</button>
+        <router-link to="/register">Register as a Coach</router-link>
+      </div>
+      <ul>LIST OF COACHES</ul>
     </section>
+  </div>
 </template>
 
 <script>
