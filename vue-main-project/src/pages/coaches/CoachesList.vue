f20f303ade304680f8fa1f2a6f392a04acedaa4a
Joonwoo Park <joon@Joonwoos-MacBook-Pro.local>
2021-07-28 11:52:51 +0900

    done with Coach List

--- vue-main-project/src/pages/coaches/CoachesList.vue
+++ vue-main-project/src/pages/coaches/CoachesList.vue
@@ -8,13 +8,38 @@
         <button>Refresh</button>
         <router-link to="/register">Register as a Coach</router-link>
       </div>
-      <ul>LIST OF COACHES</ul>
+      <ul v-if="hasCoaches">
+        <coach-item 
+          v-for="coach in filteredCoaches" 
+          :key="coach.id"
+          :id="coach.id"
+          :first-name="coach.firstName"
+          :last-name="coach.lastName"
+          :rate="coach.hourlyRate"
+          :areas="coach.areas"
+        ></coach-item>
+      </ul>
+      <h3 v-else>No coaches found ..</h3>
     </section>
   </div>
 </template>
 
 <script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
 
+export default {
+  components: {
+    CoachItem
+  },
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    }
+  }
+}
 </script>
 
 <style>
@@ -30,5 +55,18 @@ html {
 
 body {
   margin: 0;
+}
+</style>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
 }
 </style>
